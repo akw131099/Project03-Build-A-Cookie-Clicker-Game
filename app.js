9dbe0c329c351ba78138ef8177f04dcd8f3026ad
Alex Kieran Wright <alex.092@live.com>
2024-12-19 14:23:04 +0000

    successfully rendered content from API on to document

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ let gameData = {
   patsPerSecond: 0,
 };
 
+const shopBox = document.getElementById("shop-box");
+const dogClickerButton = document.getElementById("dogclicker");
+
 async function getUpgrades() {
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
   );
   //   console.log(response);
-  const jsonUpgrades = await response.json();
+  let jsonUpgrades = await response.json();
   //   console.log(jsonUpgrades);
 
-  const shopItems = [];
+  let shopItems = [];
   shopItems.push(jsonUpgrades);
   console.log(shopItems);
+
+  function renderUpgrades() {
+    for (let i = 0; i < shopItems[0].length; i++) {
+      console.log(i);
+      const upgradeItem = document.createElement("h3");
+      upgradeItem.textContent = shopItems[0][i].name;
+      shopBox.appendChild(upgradeItem);
+    }
+  }
+  renderUpgrades();
 }
 getUpgrades();
