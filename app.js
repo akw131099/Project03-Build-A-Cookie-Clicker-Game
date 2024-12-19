f79f9f27a8c917233a42e6c954056dbf6b442436
Alex Kieran Wright <alex.092@live.com>
2024-12-19 13:20:26 +0000

    fetched from API, parsed and pushed to array

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // console.log("test");
+let gameData = {
+  pats: 0,
+  patsPerSecond: 0,
+};
+
 async function getUpgrades() {
   const response = await fetch(
     "https://cookie-upgrade-api.vercel.app/api/upgrades"
   );
   //   console.log(response);
   const jsonUpgrades = await response.json();
-  console.log(jsonUpgrades);
+  //   console.log(jsonUpgrades);
+
+  const shopItems = [];
+  shopItems.push(jsonUpgrades);
+  console.log(shopItems);
 }
 getUpgrades();
