d823df5104e70d59d9cc2ab9c98148faee6afc24
Alex Kieran Wright <alex.092@live.com>
2024-12-19 10:55:09 +0000

    fetched upgrades from API

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // console.log("test");
+async function getUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  //   console.log(response);
+  const jsonUpgrades = await response.json();
+  console.log(jsonUpgrades);
+}
+getUpgrades();
